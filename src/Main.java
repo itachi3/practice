92f98605a6d487c761ef6db80119e5c432c3f7a3
itachi <G@Balajis-MacBook-Pro-2.local>
2017-07-01 12:28:36 +0530

    Sorting photos

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ import java.util.*;
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-        Integer arr[] = {13, 3, 14, 1, 4, 18, 2, 12, 10, 4, 4, 4, 5, 11, 8, 7, 9, 6};
+        //Integer arr[] = {13, 3, 14, 1, 4, 18, 2, 12, 10, 4, 4, 4, 5, 11, 8, 7, 9, 6};
         //modulesBST(arr);
-        moduleList(arr);
+        //moduleList(arr);
         //moduleMinHeap(arr);
         //moduleArray(arr);
         //moduleDP(arr);
@@ -15,6 +15,21 @@ public class Main {
         //Integer graphMatrix[][] = {{0, 0, 1, 1, 0}, {1, 0, 0, 0, 0}, {0, 1, 0, 0, 0}, {0, 0, 0, 0, 4}, {0, 0, 0, 0, 0}};
         //moduleGraph(graphMatrix);
         //moduleString();
+        Strings.Solution("photo.jpg, Warsaw, 2013-09-05 14:08:15\n" +
+                "john.png, London, 2015-06-20 15:13:22\n" +
+                "myFriends.png, Warsaw, 2013-09-05 14:07:13\n" +
+                "Eiffel.jpg, Paris, 2015-07-23 08:03:02\n" +
+                "pisatower.jpg, Paris, 2015-07-22 23:59:59\n" +
+                "BOB.jpg, London, 2015-08-05 00:02:03\n" +
+                "notredame.png, Paris, 2015-09-01 12:00:00\n" +
+                "me.jpg, Warsaw, 2013-09-06 15:40:22\n" +
+                "a.png, Warsaw, 2016-02-13 13:33:50\n" +
+                "b.jpg, Warsaw, 2016-01-02 15:12:22\n" +
+                "c.jpg, Warsaw, 2016-01-02 14:34:30\n" +
+                "d.jpg, Warsaw, 2016-01-02 15:15:01\n" +
+                "e.png, Warsaw, 2016-01-02 09:49:09\n" +
+                "f.png, Warsaw, 2016-01-02 10:55:32\n" +
+                "g.jpg, Warsaw, 2016-02-29 22:13:11");
     }
 
     static void moduleLRUCache(Integer arr[]) {
